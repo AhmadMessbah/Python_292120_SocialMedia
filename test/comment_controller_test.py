8e909bf75ed6e8cf12f49cc36ba90cafa65ccd9d
amirmehran <rezamehran062@gmail.com>
2024-02-05 13:33:35 -0800

    Comment Controller was inititatred completed and tested succesfully.  -Amirreaza

---
+++ test/comment_controller_test.py
@@ -0,0 +1,9 @@
+# # from controller.like_controller import LikeController
+# from controller.comment_controller import CommentController
+# from controller.post_controller import PostController
+# from controller.user_controller import UserController
+#
+# #
+# # #uc = UserController()
+# c_c= CommentController()
+# # print(c_c.find_by_text("lololo good !!!"))
