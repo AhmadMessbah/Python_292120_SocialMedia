335b5b06f951d5ee9721255585f72e78db918fde
amirmehran <rezamehran062@gmail.com>
2024-02-07 02:15:53 -0800

    all functions in Like controller was defined and tunned and tessted successfullyly.-Amirreaza

--- test/org_Like_Controller_testing.py
+++ test/org_Like_Controller_testing.py
@@ -1,9 +1,15 @@
-# from controller.like_controller import LikeController
+# # from controller.like_controller import LikeController
+# # from controller.post_controller import PostController
+# # from controller.user_controller import UserController
+# #
 # from controller.post_controller import PostController
-# from controller.user_controller import UserController
 #
 # pc=PostController()
-# uc=UserController()
-# lc=LikeController()
-# # print(pc.remove(8))
-# # print(lc.find_by_post(pc.find_by_id_internal(270)))
+# # print(pc.remove(1))
+# # from controller.user_controller import UserController
+# #
+# # uc=UserController()
+# # print(uc.remove(16))
+# # lc=LikeController()
+# # # print(pc.remove(8))
+# # # print(lc.find_by_post(pc.find_by_id_internal(270)))
